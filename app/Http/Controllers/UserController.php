d7b6b663757b838a5689e55af8ac1703735b751a
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-09-01 08:19:15 +0700

    Membuat login page dan menjalankan unit test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
     // login page
-    public function login() {}
+    public function login(): Response
+    {
+        // kembalikan view login
+        return response()->view("user.login", ["title" => "Login"]);
+    }
 
     // aksi untuk login
     public function doLogin() {}
